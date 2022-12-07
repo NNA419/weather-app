d4f2cc763a514869285b9490ac198686dd33b51d
NNA419 <aby.ngoc.anh@gamil.com>
2022-12-07 19:24:55 +0700

    update new solotion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { cleanup } from '@testing-library/react';
+import React, { useState, useEffect } from 'react';
+
+const api = {
+  key: "5ba3cd1a540ff877629673a2ab7be868",
+  base: "https://pro.openweathermap.org/data/2.5/",
+};
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+    const [searchInput, setSearchInput] = useState("");
+    const [searchCity, setSearchCity] = useState("");
+    const [weatherInfo, setWeatherInfo] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(false);
+   
+    useEffect(() => {
+        const fetchWeatherData = async () => {
+            if (!searchCity) return;
+            setLoading(true);
+            //Process
+            try {
+                const url = `${api.base}forecast/hourly?q=${searchCity}&appid=${api.key}`
+                const response = await fetch(url);
+                const data = await response.json();
+                setWeatherInfo(JSON.stringify(data))
+            } catch (error) {
+                setErrorMessage(error.message);
+            }
+            setLoading(false);
+        };
+        fetchWeatherData();
+
+    }, [searchCity]);
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSearchCity(searchInput);
+    }
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder='City'
+                    value={searchInput}
+                    onChange={(e) => setSearchInput(e.target.value)} />
+                <button>Search</button>
+            </form>
+            <div>{weatherInfo}</div>
+        </>
+    );
+};
 
 export default App;
